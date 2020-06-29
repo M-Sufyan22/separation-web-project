1c2107557e08f3bec32dafccbcaa1f1a6f8f5245
M-Sufyan22 <mmuhammadsufyan5@gmail.com>
2020-06-30 00:53:34 +0500

    separation web uploaded on git repo

---
+++ assets/js/app.js
@@ -0,0 +1,40 @@
+document.querySelector("#icon").addEventListener("click", function() {
+    document.querySelector("#nav").classList.toggle("show");
+    document.querySelector("#icon").classList.toggle("icon-move");
+    // document.querySelector("#nav").classList.add("container-shift");
+})
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("header").style.top = "0";
+    } else {
+        document.getElementById("header").style.top = "-100px";
+    }
+    prevScrollpos = currentScrollPos;
+}
+
+
+
+var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+})
