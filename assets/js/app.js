f60dc030bcb1eb49ce894d6759cbe9e52ebb7e06
M-Sufyan22 <mmuhammadsufyan5@gmail.com>
2020-06-30 23:27:29 +0500

    home-page completed

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,19 +4,33 @@ document.querySelector("#icon").addEventListener("click", function() {
     // document.querySelector("#nav").classList.add("container-shift");
 })
 
+
+// mybutton = ;
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
-    var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-        document.getElementById("header").style.top = "0";
-    } else {
-        document.getElementById("header").style.top = "-100px";
+        var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+            document.getElementById("header").style.top = "0";
+        } else {
+            document.getElementById("header").style.top = "-100px";
+        }
+        if (prevScrollpos < currentScrollPos) {
+            document.getElementById("myBtn").style.display = "block";
+        } else {
+            document.getElementById("myBtn").style.display = "none";
+        }
+        prevScrollpos = currentScrollPos;
+
     }
-    prevScrollpos = currentScrollPos;
+    // go to top function
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
 
 
+// 
 var mySwiper = new Swiper('.swiper-container', {
     // Optional parameters
     direction: 'horizontal',
